fe3f78cff34082115811903472d6bb91634f5bfc
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-01 04:54:09 -0800

    task9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_count = 0;
+	size_t right_count = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		if (tree)
+		{
+			left_count = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_count = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((left_count > right_count) ? left_count : right_count);
+	}
+}
