bd241de95601922976fe9fdc4026afe0bd9d8339
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-01 05:28:20 -0800

    task17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds if a node is sibling
+ * @node: node to check
+ * Return: sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
