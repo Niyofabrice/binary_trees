bfa48e9f610537fac7978225457808135dba000f
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-02-01 04:47:06 -0800

    task5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,18 +5,9 @@
  * @node: Node to check
  * Return: 0 if is not a root 1 if it is
  */
-int binary_tree_is_root(const binary_tree_t *node){
-	if(node == NULL)
-	{
-		return 0;
-	}
-
-	if(node->parent == NULL)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
 }
